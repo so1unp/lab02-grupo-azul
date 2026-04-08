90f3254c1853abfb866e0bb9e44fecf3ed4ac26a
SolaNicolas <nicolas.solaleiva@gmail.com>
2026-04-08 15:42:07 -0300

    Punto 1 y 2 (Luka y Nico)

--- hola.c
+++ hola.c
@@ -1,11 +1,20 @@
 /*
  * Imprime un saludo por la salida estándar.
  */
+#include <stdlib.h>
+#include <stdio.h>
 
-#include <stdlib.h>     // exit()
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Termina la ejecución del proceso.
+    if (argc < 2) {
+        printf("No se proporcionó un nombre.\n");
+        exit(EXIT_FAILURE);
+    } else if (argc > 2) {
+        printf("Mucho argumento!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    char *nombre = argv[1];
+    printf("Hola %s\n", nombre);
     exit(EXIT_SUCCESS);
 }
